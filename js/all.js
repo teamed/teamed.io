bbaa9624154e3ed110de7f087052b3bbf0ff3c48
Yegor Bugayenko <yegor@tpc2.com>
2014-08-12 10:28:48 +0200

    all.js

---
+++ js/all.js
@@ -0,0 +1,29 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1963507-34']);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+(function (d, w, c) {
+  (w[c] = w[c] || []).push(function() {
+    try {
+      w.yaCounter25693490 = new Ya.Metrika({id:25693490,
+        webvisor:true,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true});
+    } catch(e) { }
+  });
+  var n = d.getElementsByTagName("script")[0],
+    s = d.createElement("script"),
+    f = function () { n.parentNode.insertBefore(s, n); };
+  s.type = "text/javascript";
+  s.async = true;
+  s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+  if (w.opera == "[object Opera]") {
+    d.addEventListener("DOMContentLoaded", f, false);
+  } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
