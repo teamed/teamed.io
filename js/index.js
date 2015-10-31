9c1afa80803d0f6f3003ae0f2e395515666ab0fe
Yegor Bugayenko <yegor@tpc2.com>
2015-10-30 17:38:34 -0700

    typo

--- js/index.js
+++ js/index.js
@@ -9,8 +9,8 @@ angular.module('teamed', []).controller(
         email(form, 'patrick@teamed.io');
         var text =
           'I have a project to outsource:\n\n'
-          + 'Contact: ' + $scope.details + '\n\n'
-          + 'Details:' + $scope.details,
+          + 'Contact: ' + $scope.contact + '\n\n'
+          + 'Details: ' + $scope.details,
         url = 'http://www.netbout.com/start?post='
           + encodeURIComponent(text)
           + '&invite=yegor256&invite=patrickboyle&rename=Estimate+for+New+Teamed+Project';
