9b22d5d2d49480dc90329b07c8fc8c57b8961c67
Yegor Bugayenko <yegor@tpc2.com>
2015-12-30 23:20:01 +0200

    me only

--- js/index.js
+++ js/index.js
@@ -6,16 +6,15 @@ angular.module('teamed', []).controller(
     function($scope) {
       'use strict';
       $scope.submit = function(form) {
-        email(form, 'patrick@teamed.io');
+        email(form, 'hire@teamed.io');
         var text =
           'I have a project to outsource:\n\n'
           + 'Contact: ' + $scope.contact + '\n\n'
           + 'Details: ' + $scope.details,
-        entry = '@patrickboyle please help us to estimate this+project',
+        entry = '@yegor256 please help us to estimate this+project',
         url = 'http://www.netbout.com/start?'
           + 'rename=Estimate+for+New+Teamed+Project'
           + '&invite=yegor256'
-          + '&invite=patrickboyle'
           + '&post=' + encodeURIComponent(text)
           + '&post=' + encodeURIComponent(entry);
         window.location = url;
